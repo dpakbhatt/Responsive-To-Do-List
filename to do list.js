d2b1fc948beef2826ec64a9488be0aa72eaa578a
dpakbhatt <deepakbhatt912@gmail.com>
2020-06-11 03:09:26 +0530

    first commit

---
+++ to do list.js
@@ -0,0 +1,12 @@
+$("#add").on("click", function() {
+    var val = $("input").val();
+    if(val !== '') {
+        var elem = $("<li></li>").text(val);
+        $(elem).append("<button class='rem'>X</button>");
+        $("#mylist").append(elem);
+        $("input").val("");
+        $(".rem").on("click", function() {
+            $(this).parent().remove();
+        });
+    }
+});
